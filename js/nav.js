dc0ba7791fb562ea3af2d3d129098bb04499cda2
Yuikyu <96453436+Yuikyu@users.noreply.github.com>
2022-01-24 21:17:09 -0300

    changes

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,12 @@
 const pnav = document.querySelector('.navi-bar');
 const tnav = document.querySelector('.navi-button');
 
+pnav.addEventListener("click", function () {
+    const visibility = pnav.getAttribute('data-visible');
+    if (visibility === "true"){
+        print(event.target)}
+});
+
 tnav.addEventListener("click", function () {
         const visibility = pnav.getAttribute('data-visible');
         if (visibility === "false"){
